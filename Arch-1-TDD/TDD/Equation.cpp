b0569eb752193a87ebdaba57e877aba1613a1801
feschenko-nelya <feschenko_n_s@list.ru>
2024-10-27 18:15:06 +0300

    Реализация теста об отсутствии корней

--- Arch-1-TDD/TDD/Equation.cpp
+++ Arch-1-TDD/TDD/Equation.cpp
@@ -1,5 +1,7 @@
 #include "Equation.h"
 
+#include <cstdlib>
+
 const double Equation::E {0.000001};
 
 Equation::Equation()
@@ -10,4 +12,18 @@ Equation::Equation()
 const std::vector<double> &Equation::getRoots() const
 {
     return _roots;
+}
+
+bool Equation::solve(const double a, const double b, const double c)
+{
+    _roots.clear();
+
+    double D = b * b - 4 * a * c;
+
+    if (std::abs(D) < E)
+    {
+        return true;
+    }
+
+    return true;
 }

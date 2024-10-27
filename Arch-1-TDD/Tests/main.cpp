41f36c913d525dc525ac9af66b1d41e99552769e
feschenko-nelya <feschenko_n_s@list.ru>
2024-10-27 20:26:24 +0300

    Тест, который проверяет, что для уравнения x^2-1 = 0 есть два корня кратности 1 (x1=1, x2=-1)

--- Arch-1-TDD/Tests/main.cpp
+++ Arch-1-TDD/Tests/main.cpp
@@ -12,6 +12,20 @@ TEST(TestGroup, SubTest_x2p1)
     ASSERT_TRUE(eqt.solve(1, 0, 1).size() == 0);
 }
 
+TEST(TestGroup, SubTest_TwoRoots)
+{
+    Equation eqt;
+
+    // x^2 - 1 = 0
+    // Два корня.
+
+    std::vector<double> roots = eqt.solve(1, 0, -1);
+    ASSERT_TRUE(roots.size() == 2);
+
+    ASSERT_TRUE(roots[0] == 1.0);
+    ASSERT_TRUE(roots[1] == -1.0);
+}
+
 int main(int argc, char *argv[])
 {
     ::testing::InitGoogleTest(&argc, argv);

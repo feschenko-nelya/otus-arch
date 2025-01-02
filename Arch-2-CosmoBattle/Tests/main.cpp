c417bf2b29439b3cced5a09196b99d1bc554d7d5
feschenko-nelya <feschenko_n_s@list.ru>
2025-01-02 18:31:07 +0300

    Tests for UObject

---
+++ Arch-2-CosmoBattle/Tests/main.cpp
@@ -0,0 +1,44 @@
+#include <gtest/gtest.h>
+
+TEST(TestGroup, TestPropertyExceptions)
+{
+    UObject obj;
+
+    std::any value;
+
+    ASSERT_THROW(obj.getProperty("value"), UObjectException);
+
+    obj.setProperty("value", 5);
+
+    ASSERT_NO_THROW(obj.getProperty("value"), UObjectException);
+}
+
+TEST(TestGroup, TestPropertyExist)
+{
+    UObject obj;
+
+    obj.setProperty("value", 5);
+
+    const std::any &value = obj.getProperty("value");
+
+    ASSERT_TRUE(value.has_value() == true);
+    ASSERT_TRUE(std::any_cast<int>(value) == 5);
+}
+
+TEST(TestGroup, TestIntProperty)
+{
+    UObject obj;
+
+    obj.setProperty("value", 5);
+
+    int value = obj.getPropertyInt("value");
+
+    ASSERT_TRUE(value == 5);
+}
+
+int main(int argc, char *argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}

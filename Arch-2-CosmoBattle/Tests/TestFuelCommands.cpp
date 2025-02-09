9777d59c16e13f1576261bdffe4ab25ee92357bf
feschenko-nelya <feschenko_n_s@list.ru>
2025-02-09 20:30:48 +0300

    Тест команды, сжигающей горючее

--- Arch-2-CosmoBattle/Tests/TestFuelCommands.cpp
+++ Arch-2-CosmoBattle/Tests/TestFuelCommands.cpp
@@ -23,3 +23,22 @@ TEST(CheckFuelCommand, Execute)
 
     EXPECT_EQ(vehicle->getFuelLevel().value, 0);
 }
+
+TEST(BurnFuelCommand, Execute)
+{
+    auto object = std::make_shared<UObject>();
+
+    auto vehicle = std::make_shared<VehicleObject>(object);
+    vehicle->setFuelLevel({100});
+
+    auto cmd = std::make_shared<BurnFuelCommand>(vehicle);
+    ASSERT_NO_THROW(cmd->execute());
+
+    EXPECT_EQ(vehicle->getFuelLevel().value, 99);
+
+    vehicle->setFuelLevel({0});
+
+    ASSERT_THROW(cmd->execute(), ObjectHasNoFuelException);
+
+    EXPECT_EQ(vehicle->getFuelLevel().value, 0);
+}
